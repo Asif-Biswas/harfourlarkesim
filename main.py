1b657ae63a739318c63ca7c604d68c4564faada6
Asif Biswas <asifbiswas85@gmail.com>
2023-02-06 22:35:17 +0600

    change

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 def create_mapping_list(strings, regexes):
-    result = []
-    for regex in regexes:
-        result.append((regex, tuple(strings)))
-    return result
+    if not isinstance(strings, list) or not isinstance(regexes, list):
+        raise ValueError("Input must be a list of strings.")
+
+    return [(regex, tuple(strings)) for regex in regexes]
+
 
 def main():
     strings = ['string1', 'string2', 'string3']
     regexes = ['regex1', 'regex2', 'regex3']
-    result = create_mapping_list(strings, regexes)
-    print(result)
+    try:
+        result = create_mapping_list(strings, regexes)
+    except ValueError as e:
+        print(e)
+    else:
+        print(result)
+
 
 if __name__ == '__main__':
     main()
--- test.py
+++ test.py
@@ -1,16 +1,38 @@
-import unittest
+import pytest
 from main import create_mapping_list
 
-class TestCreateMappingList(unittest.TestCase):
-    def test_create_mapping_list(self):
-        strings = ['string1', 'string2', 'string3']
-        regexes = ['regex1', 'regex2', 'regex3']
-        expected_output = [('regex1', ('string1', 'string2', 'string3')), \
-                           ('regex2', ('string1', 'string2', 'string3')), \
-                            ('regex3', ('string1', 'string2', 'string3'))]
-        
-        result = create_mapping_list(strings, regexes)
-        self.assertEqual(expected_output, result)
+def test_create_mapping_list_valid_input():
+    strings = ['string1', 'string2', 'string3']
+    regexes = ['regex1', 'regex2', 'regex3']
+    result = create_mapping_list(strings, regexes)
+    expected_result = [('regex1', ('string1', 'string2', 'string3')),
+                       ('regex2', ('string1', 'string2', 'string3')),
+                       ('regex3', ('string1', 'string2', 'string3'))]
+    assert result == expected_result
 
-if __name__ == '__main__':
-    unittest.main()
+def test_create_mapping_list_invalid_input():
+    strings = 'string1'
+    regexes = ['regex1', 'regex2', 'regex3']
+    with pytest.raises(ValueError, match='Input must be a list of strings.'):
+        create_mapping_list(strings, regexes)
+
+def test_create_mapping_list_empty_input():
+    strings = []
+    regexes = []
+    result = create_mapping_list(strings, regexes)
+    expected_result = []
+    assert result == expected_result
+
+def test_create_mapping_list_invalid_input_type():
+    strings = ['string1', 'string2', 'string3']
+    regexes = 'regex1'
+    with pytest.raises(ValueError, match='Input must be a list of strings.'):
+        create_mapping_list(strings, regexes)
+
+def test_create_mapping_list_empty_input_type():
+    strings = ''
+    regexes = ''
+    with pytest.raises(ValueError, match='Input must be a list of strings.'):
+        create_mapping_list(strings, regexes)
+
+
