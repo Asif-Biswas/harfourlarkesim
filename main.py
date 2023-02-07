ccbdfee1e9816524a0081f03caf5240967fa80d7
Asif Biswas <asifbiswas85@gmail.com>
2023-02-07 10:17:47 +0600

    regex

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
+import re
+
+
 def create_mapping_list(strings, regexes):
     if not isinstance(strings, list) or not isinstance(regexes, list):
         raise ValueError("Input must be a list of strings.")
 
-    return [(regex, tuple(strings)) for regex in regexes]
+    if not all(isinstance(string, str) for string in strings) or \
+            not all(isinstance(regex, str) for regex in regexes):
+        raise ValueError("Input must be a list of strings.")
+
+    result = []
+    for regex in regexes:
+        matches = (string for string in strings if re.search(regex, string))
+        result.append((regex, tuple(matches)))
+
+    return result
 
 
 def main():
-    strings = ['string1', 'string2', 'string3']
-    regexes = ['regex1', 'regex2', 'regex3']
+    strings = ['abcde', 'fghij', 'klmnoabc9', 'abc', 'hij']
+    regexes = ['abc', 'hij', 'klmno', 'bcdno']
     try:
         result = create_mapping_list(strings, regexes)
     except ValueError as e:
@@ -18,4 +30,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
--- test.py
+++ test.py
@@ -1,38 +1,61 @@
 import pytest
 from main import create_mapping_list
 
-def test_create_mapping_list_valid_input():
-    strings = ['string1', 'string2', 'string3']
-    regexes = ['regex1', 'regex2', 'regex3']
-    result = create_mapping_list(strings, regexes)
-    expected_result = [('regex1', ('string1', 'string2', 'string3')),
-                       ('regex2', ('string1', 'string2', 'string3')),
-                       ('regex3', ('string1', 'string2', 'string3'))]
-    assert result == expected_result
 
-def test_create_mapping_list_invalid_input():
-    strings = 'string1'
-    regexes = ['regex1', 'regex2', 'regex3']
-    with pytest.raises(ValueError, match='Input must be a list of strings.'):
+def test_create_mapping_list_input_not_list():
+    strings = 'abcde'
+    regexes = ['abc', 'hij', 'klmno', 'btgrc']
+    with pytest.raises(ValueError, match="Input must be a list of strings."):
         create_mapping_list(strings, regexes)
+
+    strings = ['abcde']
+    regexes = 'abc'
+    with pytest.raises(ValueError, match="Input must be a list of strings."):
+        create_mapping_list(strings, regexes)
+
+
+def test_create_mapping_list_input_not_string():
+    strings = ['abcde', 'fghij', 9, 'abc', 'hij']
+    regexes = ['abc', 'hij', 'klmno', 'btgrc']
+    with pytest.raises(ValueError, match="Input must be a list of strings."):
+        create_mapping_list(strings, regexes)
+
+    strings = ['abcde', 'fghij', 'klmnoabc9', 'abc', 'hij']
+    regexes = ['abc', 'hij', 'klmno', 9]
+    with pytest.raises(ValueError, match="Input must be a list of strings."):
+        create_mapping_list(strings, regexes)
+
+
+def test_create_mapping_list_output():
+    strings = ['abcde', 'fghij', 'klmnoabc9', 'abc', 'hij']
+    regexes = ['abc', 'hij', 'klmno', 'bcdno']
+    expected_output = [('abc', ('abcde', 'klmnoabc9', 'abc')),
+                       ('hij', ('fghij', 'hij')),
+                       ('klmno', ('klmnoabc9',)),
+                       ('bcdno', ())]
+    assert create_mapping_list(strings, regexes) == expected_output
+
 
 def test_create_mapping_list_empty_input():
     strings = []
     regexes = []
-    result = create_mapping_list(strings, regexes)
-    expected_result = []
-    assert result == expected_result
-
-def test_create_mapping_list_invalid_input_type():
-    strings = ['string1', 'string2', 'string3']
-    regexes = 'regex1'
-    with pytest.raises(ValueError, match='Input must be a list of strings.'):
-        create_mapping_list(strings, regexes)
-
-def test_create_mapping_list_empty_input_type():
-    strings = ''
-    regexes = ''
-    with pytest.raises(ValueError, match='Input must be a list of strings.'):
-        create_mapping_list(strings, regexes)
+    expected_output = []
+    assert create_mapping_list(strings, regexes) == expected_output
 
 
+def test_create_mapping_list_no_matches():
+    strings = ['abcde', 'fghij', 'klmnoabc9', 'abc', 'hij']
+    regexes = ['z', 'y', 'x']
+    expected_output = [('z', ()),
+                       ('y', ()),
+                       ('x', ())]
+    assert create_mapping_list(strings, regexes) == expected_output
+
+
+def test_create_mapping_list_multiple_matches():
+    strings = ['abcde', 'fghij', 'klmnoabc9', 'abc', 'hij']
+    regexes = ['ab', 'hi', 'klmno']
+    expected_output = [('ab', ('abcde', 'klmnoabc9', 'abc')),
+                       ('hi', ('fghij', 'hij')),
+                       ('klmno', ('klmnoabc9',))]
+    assert create_mapping_list(strings, regexes) == expected_output
